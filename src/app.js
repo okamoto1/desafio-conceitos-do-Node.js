0f1ad4c646b7e0d6d68731ef28e4df05c2ebebdb
Bruno Okamoto <brunookamoto11@gmail.com>
2020-08-19 00:11:32 -0300

    fixing

--- src/app.js
+++ src/app.js
@@ -10,22 +10,22 @@ app.use(express.json());
 app.use(cors());
 
 function validateRepositoryId(request, response, next) {
-  const { id } = request.params;
+    const { id } = request.params;
     if (!isUuid(id)){
         return response.status(400).json({ error: 'Invalid id'});
     }
-  next();
+    return next();
 }
 
 function findRepositoryIndex(id) {
   const repositoryIndex = repositories.findIndex(element => element.id === id);
   if (repositoryIndex === -1){
-    return response.status(400).json({error: 'Repository not found'});
+    return 'Repository not found';
   }
   return repositoryIndex;
 }
 
-app.use('/projects/:id', validateRepositoryId);
+app.use('/repositories/:id', validateRepositoryId);
 
 const repositories = [];
 
@@ -48,6 +48,9 @@ app.post("/repositories", (request, response) => {
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   repositoryIndex = findRepositoryIndex(id);
+  if (typeof repositoryIndex === 'string'){
+    return response.status(400).json({error: repositoryIndex});
+  }
   const { title, url, techs} = request.body;
   const likes = repositories[repositoryIndex].likes;
   const updatedRepository = {
@@ -58,21 +61,25 @@ app.put("/repositories/:id", (request, response) => {
     likes
   };
   repositories[repositoryIndex] = updatedRepository;
-  return response.json(repositories);
+  return response.json(updatedRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
   repositoryIndex = findRepositoryIndex(request.params.id);
+  if (typeof repositoryIndex === 'string'){
+    return response.status(400).json({error: repositoryIndex});
+  }
   repositories.splice(repositoryIndex, 1);
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", validateRepositoryId, (request, response) => {
   repositoryIndex = findRepositoryIndex(request.params.id);
-  let likes = repositories[repositoryIndex].likes;
-  likes = likes + 1;
-  repositories[repositoryIndex].likes = likes;
-  return response.status(200).send();
+  if (typeof repositoryIndex === 'string'){
+    return response.status(400).json({error: repositoryIndex});
+  }
+  repositories[repositoryIndex].likes += 1;
+  return response.status(200).json({likes: repositories[repositoryIndex].likes});
 });
 
 module.exports = app;
